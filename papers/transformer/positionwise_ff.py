32af83831eae28110cf805beb6125c871b60cd9b
toto_wng <r.news94@gmail.com>
2023-06-11 18:05:38 +0200

    add transformer encoder

---
+++ papers/transformer/positionwise_ff.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class PositionWiseFF(nn.Module):
+    def __init__(self, d_model: int, d_ff: int):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x: torch.Tensor):
+        return self.fc2(F.relu(self.fc1(x)))
